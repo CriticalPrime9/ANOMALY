d1f7ce051aa186b3ced1d86b87e66e1fa77be6d3
CriticalPrime9 <61815452+CriticalPrime9@users.noreply.github.com>
2020-08-12 18:45:12 -0400

    Update BlockInit.java

--- main/java/com/criticalprime9/anomaly/init/blocks/BlockInit.java
+++ main/java/com/criticalprime9/anomaly/init/blocks/BlockInit.java
@@ -22,32 +22,32 @@ public class BlockInit {
 	/**
 	 * Blue Granite /&/ Polished Blue Granite
 	 */
-	public static final RegistryObject<Block> BLUE_GRANITE = BLOCKS.register("blue_granite", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLUE_GRANITE_STAIRS = BLOCKS.register("blue_granite_stairs", () -> new CustomStairsBlock(BlockInit.BLUE_GRANITE.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.8F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLUE_GRANITE_WALL = BLOCKS.register("blue_granite_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLUE_GRANITE_SLAB = BLOCKS.register("blue_granite_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE = BLOCKS.register("polished_blue_granite", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE_STAIRS = BLOCKS.register("polished_blue_granite_stairs", () -> new CustomStairsBlock(BlockInit.POLISHED_BLUE_GRANITE.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.8F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE_SLAB = BLOCKS.register("polished_blue_granite_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
+	public static final RegistryObject<Block> BLUE_GRANITE = BLOCKS.register("blue_granite", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLUE_GRANITE_STAIRS = BLOCKS.register("blue_granite_stairs", () -> new CustomStairsBlock(BlockInit.BLUE_GRANITE.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLUE_GRANITE_WALL = BLOCKS.register("blue_granite_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLUE_GRANITE_SLAB = BLOCKS.register("blue_granite_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE = BLOCKS.register("polished_blue_granite", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE_STAIRS = BLOCKS.register("polished_blue_granite_stairs", () -> new CustomStairsBlock(BlockInit.POLISHED_BLUE_GRANITE.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> POLISHED_BLUE_GRANITE_SLAB = BLOCKS.register("polished_blue_granite_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
 	
 	/**
 	 * Quartered Stone Bricks
 	 */
-	public static final RegistryObject<Block> QUARTERED_STONE_BRICKS = BLOCKS.register("quartered_stone_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_STAIRS = BLOCKS.register("quartered_stone_brick_stairs", () -> new CustomStairsBlock(BlockInit.QUARTERED_STONE_BRICKS.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.8F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_SLAB = BLOCKS.register("quartered_stone_brick_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_WALL = BLOCKS.register("quartered_stone_brick_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
+	public static final RegistryObject<Block> QUARTERED_STONE_BRICKS = BLOCKS.register("quartered_stone_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_STAIRS = BLOCKS.register("quartered_stone_brick_stairs", () -> new CustomStairsBlock(BlockInit.QUARTERED_STONE_BRICKS.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_SLAB = BLOCKS.register("quartered_stone_brick_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> QUARTERED_STONE_BRICK_WALL = BLOCKS.register("quartered_stone_brick_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
 	
 	/**
 	 * Black Nether Bricks
 	 */
-	public static final RegistryObject<Block> BLACK_NETHER_BRICKS = BLOCKS.register("black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLACK_NETHER_BRICK_STAIRS = BLOCKS.register("black_nether_brick_stairs", () -> new CustomStairsBlock(BlockInit.BLACK_NETHER_BRICKS.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLACK_NETHER_BRICK_SLAB = BLOCKS.register("black_nether_brick_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLACK_NETHER_BRICK_WALL = BLOCKS.register("black_nether_brick_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> BLACK_NETHER_BRICK_FENCE = BLOCKS.register("black_nether_brick_fence", () -> new FenceBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> CRACKED_BLACK_NETHER_BRICKS = BLOCKS.register("cracked_black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
-	public static final RegistryObject<Block> CHISELED_BLACK_NETHER_BRICKS = BLOCKS.register("chiseled_black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
+	public static final RegistryObject<Block> BLACK_NETHER_BRICKS = BLOCKS.register("black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLACK_NETHER_BRICK_STAIRS = BLOCKS.register("black_nether_brick_stairs", () -> new CustomStairsBlock(BlockInit.BLACK_NETHER_BRICKS.get().getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLACK_NETHER_BRICK_SLAB = BLOCKS.register("black_nether_brick_slab", () -> new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLACK_NETHER_BRICK_WALL = BLOCKS.register("black_nether_brick_wall", () -> new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> BLACK_NETHER_BRICK_FENCE = BLOCKS.register("black_nether_brick_fence", () -> new FenceBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> CRACKED_BLACK_NETHER_BRICKS = BLOCKS.register("cracked_black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
+	public static final RegistryObject<Block> CHISELED_BLACK_NETHER_BRICKS = BLOCKS.register("chiseled_black_nether_bricks", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
 
 	/**
 	 * Diffused TNT
@@ -57,5 +57,5 @@ public class BlockInit {
 	/**
 	 * Smooth Stone Stairs
 	 */
-	public static final RegistryObject<Block> SMOOTH_STONE_STAIRS = BLOCKS.register("smooth_stone_stairs", () -> new CustomStairsBlock(Blocks.SMOOTH_STONE.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.8F).harvestLevel(0).harvestTool(ToolType.PICKAXE)));
+	public static final RegistryObject<Block> SMOOTH_STONE_STAIRS = BLOCKS.register("smooth_stone_stairs", () -> new CustomStairsBlock(Blocks.SMOOTH_STONE.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).harvestLevel(0).harvestTool(ToolType.PICKAXE).func_235861_h_()));
 }
